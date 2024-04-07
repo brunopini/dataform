884be2964f671a245facf41470f91b79ba769a72
brunopini <pini.brunomagri@gmail.com>
2024-04-06 23:39:39 -0300

    testing stats

--- definitions/staging/fct/stg_stats.js
+++ definitions/staging/fct/stg_stats.js
@@ -1,93 +1,140 @@
 const {
-    joinOn,
-} = require("includes/utils.js");
+  sourceSchemaSuffix,
+  businessUnits
+} = require('config.js');
 const {
-    primaryKeyRaw,
-    statsSelectDimensions,
-    viewConfig,
-} = require("includes/stats.js");
+  extractAttribute,
+  generateUnionAllQuery,
+  joinOn,
+} = require('includes/utils.js');
+const {
+  getNotNullColumns,
+  getPrimaryKeys,
+  generateSelectColumns,
+} = require('includes/schema.js');
+const {
+  primaryKeyRaw,
+  statsSelectDimensions,
+  viewConfig,
+  columns
+} = require('includes/stats.js');
 
 
-publish('stg_stats', viewConfig).query(ctx => `
+const columns = (ctx) => columns(ctx, 'dlv.').push([
+  { name: 'CAST(dlv.AdvertiserCost AS NUMERIC)', type: 'NUMERIC', alias: 'advertiser_cost' },
+  { name: 'CAST(dlv.Clicks AS NUMERIC)', type: 'NUMERIC', alias: 'clicks' },
+  { name: 'CAST(dlv.Displays AS NUMERIC)', type: 'NUMERIC', alias: 'displays' },
+  { name: 'CAST(dlv.NonViewableDisplays AS NUMERIC)', type: 'NUMERIC', alias: 'non_viewable_displays' },
+  { name: 'CAST(dlv.PotentialDisplays AS NUMERIC)', type: 'NUMERIC', alias: 'potential_displays' },
+  { name: 'CAST(dlv.UntrackableDisplays AS NUMERIC)', type: 'NUMERIC', alias: 'untrackable_displays' },
+  { name: 'CAST(dlv.ViewableDisplays AS NUMERIC)', type: 'NUMERIC', alias: 'viewable_displays' },
+  { name: 'CAST(app.AppInstalls AS NUMERIC)', type: 'NUMERIC', alias: 'app_installs' },
+  { name: 'CAST(app.PostInstallSales AS NUMERIC)', type: 'NUMERIC', alias: 'post_install_sales' },
+  { name: 'CAST(app.PostInstallOrderValue AS NUMERIC)', type: 'NUMERIC', alias: 'post_install_order_value' },
+  { name: 'CAST(crt.AverageCartAllClientAttribution AS NUMERIC)', type: 'NUMERIC', alias: 'avg_cart_all_client_attribution' },
+  { name: 'CAST(crt.AverageCartAllPc1d AS NUMERIC)', type: 'NUMERIC', alias: 'avg_cart_all_pc1d' },
+  { name: 'CAST(crt.AverageCartAllPc7d AS NUMERIC)', type: 'NUMERIC', alias: 'avg_cart_all_pc7d' },
+  { name: 'CAST(crt.AverageCartAllPc30d AS NUMERIC)', type: 'NUMERIC', alias: 'avg_cart_all_pc30d' },
+  { name: 'CAST(crt.AverageCartAllPc30dPv24h AS NUMERIC)', type: 'NUMERIC', alias: 'avg_cart_all_pc30d_pv24h' },
+  { name: 'CAST(crt.AverageCartAllPv24h AS NUMERIC)', type: 'NUMERIC', alias: 'avg_cart_all_pv24h' },
+  { name: 'CAST(crt.AverageCartClientAttribution AS NUMERIC)', type: 'NUMERIC', alias: 'avg_cart_client_attribution' },
+  { name: 'CAST(crt.AverageCartPc1d AS NUMERIC)', type: 'NUMERIC', alias: 'avg_cart_pc1d' },
+  { name: 'CAST(crt.AverageCartPc7d AS NUMERIC)', type: 'NUMERIC', alias: 'avg_cart_pc7d' },
+  { name: 'CAST(crt.AverageCartPc30d AS NUMERIC)', type: 'NUMERIC', alias: 'avg_cart_pc30d' },
+  { name: 'CAST(crt.AverageCartPc30dPv24h AS NUMERIC)', type: 'NUMERIC', alias: 'avg_cart_pc30d_pv24h' },
+  { name: 'CAST(crt.AverageCartPv24h AS NUMERIC)', type: 'NUMERIC', alias: 'avg_cart_pv24h' },
+  { name: 'CAST(sls.OmnichannelSalesAllPc30d AS NUMERIC)', type: 'NUMERIC', alias: 'omni_sales_all_pc30d' },
+  { name: 'CAST(sls.OmnichannelSalesAllPv24h AS NUMERIC)', type: 'NUMERIC', alias: 'omni_sales_all_pv24h' },
+  { name: 'CAST(sls.OmnichannelSalesPc30d AS NUMERIC)', type: 'NUMERIC', alias: 'omni_sales_pc30d' },
+  { name: 'CAST(sls.OmnichannelSalesPv24h AS NUMERIC)', type: 'NUMERIC', alias: 'omni_sales_pv24h' },
+  { name: 'CAST(sls.OmnichannelSalesClientAttribution AS NUMERIC)', type: 'NUMERIC', alias: 'omni_sales_client_attribution' },
+  { name: 'CAST(sls.SalesAllPc1d AS NUMERIC)', type: 'NUMERIC', alias: 'sales_all_pc1d' },
+  { name: 'CAST(sls.SalesAllPc7d AS NUMERIC)', type: 'NUMERIC', alias: 'sales_all_pc7d' },
+  { name: 'CAST(sls.SalesAllPc30d AS NUMERIC)', type: 'NUMERIC', alias: 'sales_all_pc30d' },
+  { name: 'CAST(sls.SalesAllPc30dPv24h AS NUMERIC)', type: 'NUMERIC', alias: 'sales_all_pc30d_pv24h' },
+  { name: 'CAST(sls.SalesAllPv24h AS NUMERIC)', type: 'NUMERIC', alias: 'sales_all_pv24h' },
+  { name: 'CAST(sls.SalesClientAttribution AS NUMERIC)', type: 'NUMERIC', alias: 'sales_client_attribution' },
+  { name: 'CAST(sls.SalesOfflinePc30d AS NUMERIC)', type: 'NUMERIC', alias: 'sales_offline_pc30d' },
+  { name: 'CAST(sls.SalesOfflinePv24h AS NUMERIC)', type: 'NUMERIC', alias: 'sales_offline_pv24h' },
+  { name: 'CAST(sls.SalesPc1d AS NUMERIC)', type: 'NUMERIC', alias: 'sales_pc1d' },
+  { name: 'CAST(sls.SalesPc7d AS NUMERIC)', type: 'NUMERIC', alias: 'sales_pc7d' },
+  { name: 'CAST(sls.SalesPc30d AS NUMERIC)', type: 'NUMERIC', alias: 'sales_pc30d' },
+  { name: 'CAST(sls.SalesPc30dPv24h AS NUMERIC)', type: 'NUMERIC', alias: 'sales_pc30d_pv24h' },
+  { name: 'CAST(sls.SalesPv24h AS NUMERIC)', type: 'NUMERIC', alias: 'sales_pv24h' },
+  { name: 'CAST(rvn.AdvertiserAllValue AS NUMERIC)', type: 'NUMERIC', alias: 'advertiser_all_value' },
+  { name: 'CAST(rvn.AdvertiserValue AS NUMERIC)', type: 'NUMERIC', alias: 'advertiser_value' },
+  { name: 'CAST(rvn.OmnichannelRevenueAllPc30d AS NUMERIC)', type: 'NUMERIC', alias: 'omni_revenue_all_pc30d' },
+  { name: 'CAST(rvn.OmnichannelRevenueAllPv24h AS NUMERIC)', type: 'NUMERIC', alias: 'omni_revenue_all_pc24h' },
+  { name: 'CAST(rvn.OmnichannelRevenuePc30d AS NUMERIC)', type: 'NUMERIC', alias: 'omni_revenue_pc30d' },
+  { name: 'CAST(rvn.OmnichannelRevenuePv24h AS NUMERIC)', type: 'NUMERIC', alias: 'omni_revenue_pv24h' },
+  { name: 'CAST(rvn.RevenueGeneratedAllPc1d AS NUMERIC)', type: 'NUMERIC', alias: 'revenue_generated_all_pc1d' },
+  { name: 'CAST(rvn.RevenueGeneratedAllPc7d AS NUMERIC)', type: 'NUMERIC', alias: 'revenue_generated_all_pc7d' },
+  { name: 'CAST(rvn.RevenueGeneratedAllPc30d AS NUMERIC)', type: 'NUMERIC', alias: 'revenue_generated_all_pc30d' },
+  { name: 'CAST(rvn.RevenueGeneratedAllPc30dPv24h AS NUMERIC)', type: 'NUMERIC', alias: 'revenue_generated_all_pc30d_pv24h' },
+  { name: 'CAST(rvn.RevenueGeneratedAllPv24h AS NUMERIC)', type: 'NUMERIC', alias: 'revenue_generated_all_pv24h' },
+  { name: 'CAST(rvn.OmnichannelRevenueClientAttribution AS NUMERIC)', type: 'NUMERIC', alias: 'omni_revenue_client_attribution' },
+  { name: 'CAST(rvn.RevenueGeneratedOfflinePc30d AS NUMERIC)', type: 'NUMERIC', alias: 'revenue_generated_offline_pc30d' },
+  { name: 'CAST(rvn.RevenueGeneratedOfflinePv24h AS NUMERIC)', type: 'NUMERIC', alias: 'revenue_generated_offline_pc24h' },
+  { name: 'CAST(rvn.RevenueGeneratedPc1d AS NUMERIC)', type: 'NUMERIC', alias: 'revenue_generated_pc1d' },
+  { name: 'CAST(rvn.RevenueGeneratedPc7d AS NUMERIC)', type: 'NUMERIC', alias: 'revenue_generated_pc7d' },
+  { name: 'CAST(rvn.RevenueGeneratedPc30d AS NUMERIC)', type: 'NUMERIC', alias: 'revenue_generated_pc30d' },
+  { name: 'CAST(rvn.RevenueGeneratedPc30dPv24h AS NUMERIC)', type: 'NUMERIC', alias: 'revenue_generated_pc30d_pv24h' },
+  { name: 'CAST(rvn.RevenueGeneratedPv24h AS NUMERIC)', type: 'NUMERIC', alias: 'revenue_generated_pv24h' }
+])
+
+const uniqueAssertion = getPrimaryKeys(columns);
+const nonNullAssertion = getNotNullColumns(columns);
+
+const joinQuery = (ctx) => `
   SELECT
-    -- Ommit comma ,
-    ${statsSelectDimensions('dlv')}
-    -- dlvMetricsSelect
-    CAST(dlv.AdvertiserCost AS NUMERIC) AS advertiser_cost,
-    CAST(dlv.Clicks AS NUMERIC) AS clicks,
-    CAST(dlv.Displays AS NUMERIC) AS displays,
-    CAST(dlv.NonViewableDisplays AS NUMERIC) AS non_viewable_displays,
-    CAST(dlv.PotentialDisplays AS NUMERIC) AS potential_displays,
-    CAST(dlv.UntrackableDisplays AS NUMERIC) AS untrackable_displays,
-    CAST(dlv.ViewableDisplays AS NUMERIC) AS viewable_displays,
-    -- appMetricsSelect
-    CAST(app.AppInstalls AS NUMERIC) AS app_installs,
-    CAST(app.PostInstallSales AS NUMERIC) AS post_install_sales,
-    CAST(app.PostInstallOrderValue AS NUMERIC) AS post_install_order_value,
-    -- crtMetricsSelect
-    CAST(crt.AverageCartAllClientAttribution AS NUMERIC) AS avg_cart_all_client_attribution,
-    CAST(crt.AverageCartAllPc1d AS NUMERIC) AS avg_cart_all_pc1d,
-    CAST(crt.AverageCartAllPc7d AS NUMERIC) AS avg_cart_all_pc7d,
-    CAST(crt.AverageCartAllPc30d AS NUMERIC) AS avg_cart_all_pc30d,
-    CAST(crt.AverageCartAllPc30dPv24h AS NUMERIC) AS avg_cart_all_pc30d_pv24h,
-    CAST(crt.AverageCartAllPv24h AS NUMERIC) AS avg_cart_all_pv24h,
-    CAST(crt.AverageCartClientAttribution AS NUMERIC) AS avg_cart_client_attribution,
-    CAST(crt.AverageCartPc1d AS NUMERIC) AS avg_cart_pc1d,
-    CAST(crt.AverageCartPc7d AS NUMERIC) AS avg_cart_pc7d,
-    CAST(crt.AverageCartPc30d AS NUMERIC) AS avg_cart_pc30d,
-    CAST(crt.AverageCartPc30dPv24h AS NUMERIC) AS avg_cart_pc30d_pv24h,
-    CAST(crt.AverageCartPv24h AS NUMERIC) AS avg_cart_pv24h,
-    -- slsMetricsSelect
-    CAST(sls.OmnichannelSalesAllPc30d AS NUMERIC) AS omni_sales_all_pc30d,
-    CAST(sls.OmnichannelSalesAllPv24h AS NUMERIC) AS omni_sales_all_pv24h,
-    CAST(sls.OmnichannelSalesPc30d AS NUMERIC) AS omni_sales_pc30d,
-    CAST(sls.OmnichannelSalesPv24h AS NUMERIC) AS omni_sales_pv24h,
-    CAST(sls.OmnichannelSalesClientAttribution AS NUMERIC) AS omni_sales_client_attribution,
-    CAST(sls.SalesAllPc1d AS NUMERIC) AS sales_all_pc1d,
-    CAST(sls.SalesAllPc7d AS NUMERIC) AS sales_all_pc7d,
-    CAST(sls.SalesAllPc30d AS NUMERIC) AS sales_all_pc30d,
-    CAST(sls.SalesAllPc30dPv24h AS NUMERIC) AS sales_all_pc30d_pv24h,
-    CAST(sls.SalesAllPv24h AS NUMERIC) AS sales_all_pv24h,
-    CAST(sls.SalesClientAttribution AS NUMERIC) AS sales_client_attribution,
-    CAST(sls.SalesOfflinePc30d AS NUMERIC) AS sales_offline_pc30d,
-    CAST(sls.SalesOfflinePv24h AS NUMERIC) AS sales_offline_pv24h,
-    CAST(sls.SalesPc1d AS NUMERIC) AS sales_pc1d,
-    CAST(sls.SalesPc7d AS NUMERIC) AS sales_pc7d,
-    CAST(sls.SalesPc30d AS NUMERIC) AS sales_pc30d,
-    CAST(sls.SalesPc30dPv24h AS NUMERIC) AS sales_pc30d_pv24h,
-    CAST(sls.SalesPv24h AS NUMERIC) AS sales_pv24h,
-    -- rvnMetricsSelect
-    CAST(rvn.AdvertiserAllValue AS NUMERIC) AS advertiser_all_value,
-    CAST(rvn.AdvertiserValue AS NUMERIC) AS advertiser_value,
-    CAST(rvn.OmnichannelRevenueAllPc30d AS NUMERIC) AS omni_revenue_all_pc30d,
-    CAST(rvn.OmnichannelRevenueAllPv24h AS NUMERIC) AS omni_revenue_all_pc24h,
-    CAST(rvn.OmnichannelRevenuePc30d AS NUMERIC) AS omni_revenue_pc30d,
-    CAST(rvn.OmnichannelRevenuePv24h AS NUMERIC) AS omni_revenue_pv24h,
-    CAST(rvn.RevenueGeneratedAllPc1d AS NUMERIC) AS revenue_generated_all_pc1d,
-    CAST(rvn.RevenueGeneratedAllPc7d AS NUMERIC) AS revenue_generated_all_pc7d,
-    CAST(rvn.RevenueGeneratedAllPc30d AS NUMERIC) AS revenue_generated_all_pc30d,
-    CAST(rvn.RevenueGeneratedAllPc30dPv24h AS NUMERIC) AS revenue_generated_all_pc30d_pv24h,
-    CAST(rvn.RevenueGeneratedAllPv24h AS NUMERIC) AS revenue_generated_all_pv24h,
-    CAST(rvn.OmnichannelRevenueClientAttribution AS NUMERIC) AS omni_revenue_client_attribution,
-    CAST(rvn.RevenueGeneratedOfflinePc30d AS NUMERIC) AS revenue_generated_offline_pc30d,
-    CAST(rvn.RevenueGeneratedOfflinePv24h AS NUMERIC) AS revenue_generated_offline_pc24h,
-    CAST(rvn.RevenueGeneratedPc1d AS NUMERIC) AS revenue_generated_pc1d,
-    CAST(rvn.RevenueGeneratedPc7d AS NUMERIC) AS revenue_generated_pc7d,
-    CAST(rvn.RevenueGeneratedPc30d AS NUMERIC) AS revenue_generated_pc30d,
-    CAST(rvn.RevenueGeneratedPc30dPv24h AS NUMERIC) AS revenue_generated_pc1d_pv24h,
-    CAST(rvn.RevenueGeneratedPv24h AS NUMERIC) AS revenue_generated_pv24h,
+    ${generateSelectColumns(ctx, columns)}
   FROM
     ${ctx.ref('statistics_pre_click')} dlv
   JOIN
     ${ctx.ref('statistics_app')} app
-    ON ${joinOn(primaryKeyRaw, 'dlv', 'app')}
+    ON ${joinOn(uniqueAssertion, 'dlv', 'app')}
   JOIN
     ${ctx.ref('statistics_avg_cart')} crt
-    ON ${joinOn(primaryKeyRaw, 'dlv', 'crt')}
+    ON ${joinOn(uniqueAssertion, 'dlv', 'crt')}
   JOIN
     ${ctx.ref('statistics_sales')} sls
-    ON ${joinOn(primaryKeyRaw, 'dlv', 'sls')}
+    ON ${joinOn(uniqueAssertion, 'dlv', 'sls')}
   JOIN
     ${ctx.ref('statistics_revenue')} rvn
-    ON ${joinOn(primaryKeyRaw, 'dlv', 'rvn')}
-`)
+    ON ${joinOn(uniqueAssertion, 'dlv', 'rvn')}
+`
+
+function generateJoinQuery(ctx, columns, sourceSchemaSuffix, tablesToJoin, uniqueAssertion, businessUnit) {
+  // let joinQueryParts = [];
+  const baseTable = tablesToJoin.shift(); // Assuming the first table is the base for joining others
+  let baseQuery = `SELECT ${generateSelectColumns(ctx, columns)} FROM ${ctx.ref(`${businessUnit.schemaPreffix}_${sourceSchemaSuffix}`, baseTable)} `;
+  
+  tablesToJoin.forEach((table, index) => {
+      const tableAlias = `t${index + 1}`;
+      baseQuery += `
+        JOIN ${ctx.ref(`${businessUnit.schemaPreffix}_${sourceSchemaSuffix}`, table)} ${tableAlias}
+        ON ${joinOn(uniqueAssertion, baseTable, tableAlias)}\n
+      `;
+  });
+  return baseQuery;
+}
+
+businessUnits.forEach(businessUnit => {
+  publish('stg_ad', {
+      type: 'view',
+      schema: `${businessUnit.schemaPreffix}_${sourceSchemaSuffix}`,
+      assertions: {
+          uniqueKey: uniqueAssertion,
+          nonNull: nonNullAssertion
+      },
+      tags: ['staging', 'view', 'dim']
+  }).query(ctx => generateJoinQuery(
+      ctx, columns, businessUnit, sourceSchemaSuffix, ['statistics_pre_click', 'statistics_app', 'statistics_avg_cart', 'statistics_sales', 'statistics_revenue'], uniqueAssertion
+  ))
+});
+
+module.exports = {
+  columns,
+  uniqueAssertion,
+  nonNullAssertion
+}
--- includes/stats.js
+++ includes/stats.js
@@ -56,6 +56,37 @@ function statsSchemaDimensions(metricsSchema, ctx) {
     `
 };
 
+const columns = (ctx, tableAlias = '') => [
+    { name: `DATE(${tableAlias}Day)`, type: 'STRING NOT NULL', alias: 'date', constraints: [
+        'PRIMARY KEY'] },
+    { name: `${tableAlias}AdId`, type: 'STRING NOT NULL', alias: 'ad_id', constraints: [
+        'PRIMARY KEY',
+        `FOREIGN KEY (advertiser_id) ${ctx.ref('dim_ad')}(id, advertiser_id)`] },
+    { name: `${tableAlias}MarketingObjectiveId`, type: 'STRING NOT NULL', alias: 'marketing_objective_id', constraints: [
+        'PRIMARY KEY',
+        `FOREIGN KEY ${ctx.ref('dim_marketing_objective')}(id)`]},
+    { name: `${tableAlias}ChannelId`, type: 'STRING NOT NULL', alias: 'channel_id', constraints: [
+        'PRIMARY KEY',
+        `FOREIGN KEY ${ctx.ref('dim_category')}(id)`] },
+    { name: `${tableAlias}CategoryId`, type: 'STRING NOT NULL', alias: 'category_id', constraints: [
+        `FOREIGN KEY (advertiser_id) ${ctx.ref('dim_category')}(id, advertiser_id)`] },
+    { name: `${tableAlias}CouponId`, type: 'STRING NOT NULL', alias: 'coupon_id', constraints: [
+        'PRIMARY KEY',
+        `FOREIGN KEY (advertiser_id) ${ctx.ref('dim_coupon')}(id, advertiser_id)`] },
+    { name: `${tableAlias}AdvertiserId`, type: 'STRING NOT NULL', alias: 'advertiser_id', constraints: [
+        'PRIMARY KEY',
+        `FOREIGN KEY ${ctx.ref('dim_advertiser')}(id)`] },
+    { name: `${tableAlias}Device`, type: 'STRING NOT NULL', alias: 'device', constraints: [
+        'PRIMARY KEY'] },
+    { name: `${tableAlias}Os`, type: 'STRING NOT NULL', alias: 'os', constraints: [
+        'PRIMARY KEY'] },
+    { name: `${tableAlias}AdsetId`, type: 'STRING NOT NULL', alias: 'adset_id', constraints: [
+        `FOREIGN KEY (advertiser_id) ${ctx.ref('dim_adset')}(id, advertiser_id)`] },
+    { name: `${tableAlias}CampaignId`, type: 'STRING NOT NULL', alias: 'campaign_id', constraints: [
+        `FOREIGN KEY (advertiser_id) ${ctx.ref('dim_campaign')}(id, advertiser_id)`] },
+    { name: `${tableAlias}Currency`, type: 'STRING NOT NULL', alias: 'currency'},
+];
+
 function statsSelectDimensions(tableAlias = '') {
     const prefix = tableAlias ? `${tableAlias}.` : '';
 
@@ -81,4 +112,5 @@ module.exports = {
     tableConfig,
     statsSelectDimensions,
     statsSchemaDimensions,
+    columns
 };
